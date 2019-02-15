d63efc4c8d863e6122452708d8d55a0f33ba828e
PhilSym7 <philsym7@gmail.com>
2019-02-15 13:26:19 -0500

    no progress at all

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ import io
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+class MainPage(GridLayout):
+	def __init__(self, **kwargs):
+		super(MainPage, self).__init__(**kwargs)
+		self.cols = 3
+		self.rows = 3
+		self.add_widget(Button(text="Connect"))
+
+	def btnTest(self):
+		print('button pressed')
 
 class TorApp(App):
 	def build(self):
 		return MainPage()
-
-class MainPage(Widget):
-	pass
-
+	
 TorApp().run()
